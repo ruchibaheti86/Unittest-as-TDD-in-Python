bcf28353a408f40aec0598b9b4ee0070720d1fe9
ruchibaheti86 <61480735+ruchibaheti86@users.noreply.github.com>
2020-07-16 16:15:51 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+class Calculator:
+
+    def __init__(self):
+        pass
+
+    def add(self, x, y):
+        return x + y
+
+    def minus(self, x, y):
+        return x - y
+
+    def multiple(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        return x/y
